7f549842be533e0be5f64c28209f81019b49a86c
FREEDOM <freewhynane62@gmail.com>
2023-03-11 08:19:23 +0000

    added index for import and export

---
+++ src/components/index.jsx
@@ -0,0 +1,6 @@
+import DownloadQRCode from "./download/DownloadQRCode";
+import Form from "./form/Form";
+import ImageUpload from "./imageUpload/ImageUpload";
+import QRCode from "./qrcode/QRCode";
+
+export { DownloadQRCode, Form, ImageUpload, QRCode };
